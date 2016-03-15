748ade2d870047eae2e31828569cdc26f4d21800
MJ Rossetti <s2t2mail+github@gmail.com>
2016-03-15 13:57:36 -0600

    parsing commits

--- extract.rb
+++ extract.rb
@@ -76,17 +76,47 @@ end
 
 def traverse_user_repos
   @counter = 0
-  user_repos = @client.repos(@user[:login], :per_page => 100)
+  user_repos = @client.repos(@user[:login], :per_page => 100) #todo: this is responding with the user's owned repos, not all repos contributed-to...
   extract_user_repos(user_repos)
   while @client.last_response.rels[:next]
-    user_events = @client.last_response.rels[:next].get.data
-    extract_user_events(user_events)
+    user_repos = @client.last_response.rels[:next].get.data
+    extract_user_repos(user_repos)
   end
 end
 
 #
 # USER COMMITS
 #
+
+def extract_user_repo_commits(user_repos)
+  user_repos.each do |user_repo|
+    repo_full_name = user_repo[:full_name]
+    authored_commits = @client.commits(repo_full_name, {
+      :per_page => 100,
+      :author => @user[:login]
+    })
+    @counter = authored_commits.count
+    puts "REPO #{repo_full_name} -- #{@counter} COMMITS"
+    #authored_commits.each do |commit|
+    #  pp ({
+    #    :sha => commit[:sha],
+    #    :date => commit[:commit][:committer][:date] || commit[:commit][:author][:date],
+    #    :message => commit[:commit][:message]
+    #  })
+    #end
+  end
+end
+
+def traverse_user_repo_commits
+  @counter = 0
+  user_repos = @client.repos(@user[:login], :per_page => 100)
+  extract_user_repo_commits(user_repos)
+  while @client.last_response.rels[:next]
+    user_repos = @client.last_response.rels[:next].get.data
+    extract_user_repo_commits(user_repos)
+  end
+end
+
 
 #
 # TASKS
@@ -95,3 +125,4 @@ end
 extract_user
 # traverse_user_events # there are hundreds of thousands of events ...
 traverse_user_repos
+traverse_user_repo_commits
